ad436e417807d9ba007d04cc244cccccafa7e600
Luka Bri <59787168+lucabecci@users.noreply.github.com>
2020-07-15 07:11:53 -0300

    Added Build

---
+++ docs/precache-manifest.d8ee5764af3501cb194ff0845f1bd244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2c826491c2fc3482d896ea6488caa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b75888661f1e13496ae",
+    "url": "/static/css/main.34fa02b8.chunk.css"
+  },
+  {
+    "revision": "9394425a6b8574bfd50e",
+    "url": "/static/js/2.87faeb51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87faeb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b75888661f1e13496ae",
+    "url": "/static/js/main.e668a515.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
